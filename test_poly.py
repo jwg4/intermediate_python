7ae8f1a4e0426786aa713af48fc431d341ce46f1
Jack Grahl <jack@prismfp.com>
2017-09-05 11:34:55 +0100

    Add another test.

--- test_poly.py
+++ test_poly.py
@@ -8,3 +8,8 @@ class TestDisplayPoly(unittest.TestCase):
         p = Polynomial([1, 1])
         expected = "x + 1"
         self.assertEqual(str(p), expected)
+
+    def test_display_cubic(self):
+        p = Polynomial([1, 2, 2, 1])
+        expected = "x**3 + 2x**2 + 2x + 1"
+        self.assertEqual(str(p), expected)
