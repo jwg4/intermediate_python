f3edd5a826f06372ccbeeb1096c960fe80d0b87f
Jack Grahl <jack.grahl@gmail.com>
2018-04-19 22:08:32 +0000

    Changes to this file.

--- quick_examples/partitions_of_n/partitions.py
+++ quick_examples/partitions_of_n/partitions.py
@@ -1,16 +1,30 @@
-def partitions_below(n, m):
+def partitions_with_max(n, max):
+    """
+        Return all partitions of n
+        with no individual element greater than max.
+    """
     if n == 0:
+        yield []
         return
-    for i in range(m, 0, -1):
-        if i > n:
+    for first_element in range(max, 0, -1):
+        if first_element > n:
             continue
-        k = n - i
-        for p_ in partitions_below(k, i):
-            yield [i] + p_
+        k = n - first_element
+        for sub_partition in partitions_with_max(k, first_element):
+            yield [first_element] + sub_partition
 
 def partitions(n):
-    return partitions_below(n, n - 1)
+    """
+        Return a sequence of lists
+        Each element is a list of integers which sum to n - 
+        a partition n.
+        The elements of each partition are in descending order
+        and the sequence of partitions is in descending lex order.
+        >>> list(partitions(4))
+        [[3, 1], [2, 2], [2, 1, 1], [1, 1, 1, 1]]
+    """
+    return partitions_with_max(n, max=n - 1)
 
 if __name__ == '__main__':
     for p in partitions(10):
-        print(" + ".join(p))
+        print(" + ".join([ str(x) for x in p ]))
