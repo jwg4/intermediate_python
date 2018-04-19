e5ed4b66361d8f33d2f18eeb25f16b418b4a78a5
Jack Grahl <jack.grahl@gmail.com>
2018-04-19 22:11:50 +0000

    A script for the 'complementary series' A294381.
    This code is currently broken.

---
+++ quick_examples/complementary/complementary.py
@@ -0,0 +1,16 @@
+def A294381(n):
+    a = [1, 3]
+    b = [2]
+    for i in a:
+        yield i
+    for m in range(2, n):
+        a_ = a[m-1]*b[m-2]
+        yield a_
+        a.append(a_)
+        for i in range(a[m-1]+1, a_):
+            b.append(i)
+        
+    
+if __name__ == '__main__':
+    for x in A294381(20):
+        print(x)
