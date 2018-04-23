186e221bef39308b16ef0830088c106b89490c0f
Jack Grahl <jack.grahl@gmail.com>
2018-04-23 21:39:29 +0000

    Try to compare a list and a peekable wrapper.
    This is not correct, list is not itself an iterator.

--- kanas/peekable/test_peekable.py
+++ kanas/peekable/test_peekable.py
@@ -8,3 +8,8 @@ class TestPeekableInterface(unittest.TestCase):
         l = [1, 2, 3]
         p = peekable.PeekableInterface(l)
         self.assertIsNotNone(p)
+    
+    def test_next(self):
+        l = [1, 2, 3]
+        p = peekable.PeekableInterface(l)
+        self.assertEqual(l.next(), p.next())
