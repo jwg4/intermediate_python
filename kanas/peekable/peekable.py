72afa8f9e4e25f0e638fcfc532e73742383d80f3
Jack Grahl <jack.grahl@gmail.com>
2018-04-23 22:14:24 +0000

    Add a function to get the next value.

--- kanas/peekable/peekable.py
+++ kanas/peekable/peekable.py
@@ -1,15 +1,18 @@
 class PeekableInterface(object):
     def __init__(self, iterator):
         self.iterator = iterator
-        self.cache = self.iterator.__next__()
+        self.get_next_value()
 
     def peek(self):
         return self.cache
 
     def __next__(self):
         cached_value = self.cache
-        self.cache = self.iterator.__next__()
+        self.get_next_value()
         return cached_value
+
+    def get_next_value(self):
+        self.cache = self.iterator.__next__()
 
     def hasNext(self):
         pass
