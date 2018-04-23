26af63858671e5681ea96cf91f90fae204762a6b
Jack Grahl <jack.grahl@gmail.com>
2018-04-23 22:06:43 +0000

    Use two different iterators from the comparison test.

--- kanas/peekable/test_peekable.py
+++ kanas/peekable/test_peekable.py
@@ -12,6 +12,7 @@ class TestPeekableInterface(unittest.TestCase):
     
     def test_next(self):
         l = [1, 2, 3]
-        i = iter(l)
-        p = peekable.PeekableInterface(i)
-        self.assertEqual(i.__next__(), p.__next__())
+        i1 = iter(l)
+        i2 = iter(l)
+        p = peekable.PeekableInterface(i1)
+        self.assertEqual(i2.__next__(), p.__next__())
