085e84fbcf0e08ed5bcc9398d382028b668efb85
Jack Grahl <jack.grahl@gmail.com>
2018-04-23 22:08:05 +0000

    Add a passing test to check the value returned.

--- kanas/peekable/test_peekable.py
+++ kanas/peekable/test_peekable.py
@@ -10,9 +10,15 @@ class TestPeekableInterface(unittest.TestCase):
         p = peekable.PeekableInterface(i)
         self.assertIsNotNone(p)
     
-    def test_next(self):
+    def test_next_compare(self):
         l = [1, 2, 3]
         i1 = iter(l)
         i2 = iter(l)
         p = peekable.PeekableInterface(i1)
         self.assertEqual(i2.__next__(), p.__next__())
+    
+    def test_next_check_value(self):
+        l = ["foo", "bar", "baz"]
+        i = iter(l)
+        p = peekable.PeekableInterface(i)
+        self.assertEqual(p.__next__(), "foo")
