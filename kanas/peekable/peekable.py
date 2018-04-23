b4e508dc27ee0b889ee6070310516589a3f1bb6c
Jack Grahl <jack.grahl@gmail.com>
2018-04-23 22:11:28 +0000

    Actually cache the value about to appear.

--- kanas/peekable/peekable.py
+++ kanas/peekable/peekable.py
@@ -1,12 +1,15 @@
 class PeekableInterface(object):
     def __init__(self, iterator):
         self.iterator = iterator
+        self.cache = self.iterator.__next__()
 
     def peek(self):
-        pass
+        return self.cache
 
     def __next__(self):
-        return self.iterator.__next__()
+        cached_value = self.cache
+        self.cache = self.iterator.__next__()
+        return cached_value
 
     def hasNext(self):
         pass
