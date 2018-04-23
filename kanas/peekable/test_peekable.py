906a42cad927baba05829c2074c31b128fffbc40
Jack Grahl <jack.grahl@gmail.com>
2018-04-23 21:58:31 +0000

    Use iterators made from lists not just lists.

--- kanas/peekable/test_peekable.py
+++ kanas/peekable/test_peekable.py
@@ -6,10 +6,12 @@ from . import peekable
 class TestPeekableInterface(unittest.TestCase):
     def test_ctor(self):
         l = [1, 2, 3]
-        p = peekable.PeekableInterface(l)
+        i = iter(l)
+        p = peekable.PeekableInterface(i)
         self.assertIsNotNone(p)
     
     def test_next(self):
         l = [1, 2, 3]
-        p = peekable.PeekableInterface(l)
-        self.assertEqual(l.__next__(), p.__next__())
+        i = iter(l)
+        p = peekable.PeekableInterface(i)
+        self.assertEqual(i.__next__(), p.__next__())
