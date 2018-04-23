122e6b37314ccfb2410cea3fec8db7dcc2ec2e5f
Jack Grahl <jack.grahl@gmail.com>
2018-04-23 22:15:53 +0000

    Try to handle StopIteration the right way.

--- kanas/peekable/peekable.py
+++ kanas/peekable/peekable.py
@@ -1,18 +1,25 @@
 class PeekableInterface(object):
     def __init__(self, iterator):
         self.iterator = iterator
+        self.end_of_list = False
+        self.cache = None
         self.get_next_value()
 
     def peek(self):
         return self.cache
 
     def __next__(self):
+        if self.end_of_list:
+            raise StopIteration()
         cached_value = self.cache
         self.get_next_value()
         return cached_value
 
     def get_next_value(self):
-        self.cache = self.iterator.__next__()
+        try:
+            self.cache = self.iterator.__next__()
+        except StopIteration:
+            self.end_of_list = True
 
     def hasNext(self):
         pass
