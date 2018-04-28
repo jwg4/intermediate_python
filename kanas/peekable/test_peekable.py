7bf91d25fc32743bc359b932800a2df59e45b481
Jack Grahl <jack.grahl@gmail.com>
2018-04-28 22:20:56 +0000

    spacing

--- kanas/peekable/test_peekable.py
+++ kanas/peekable/test_peekable.py
@@ -9,14 +9,14 @@ class TestPeekableInterface(unittest.TestCase):
         i = iter(l)
         p = peekable.PeekableInterface(i)
         self.assertIsNotNone(p)
-    
+
     def test_next_compare(self):
         l = [1, 2, 3]
         i1 = iter(l)
         i2 = iter(l)
         p = peekable.PeekableInterface(i1)
         self.assertEqual(i2.__next__(), p.__next__())
-    
+
     def test_next_check_value(self):
         l = ["foo", "bar", "baz"]
         i = iter(l)
