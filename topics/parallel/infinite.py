3b4acb9524715cf11350cb31059944153f4d28e7
Jack Grahl <jack.grahl@gmail.com>
2018-06-10 22:42:58 +0000

    More generator stuff.

--- topics/parallel/infinite.py
+++ topics/parallel/infinite.py
@@ -1,3 +1,6 @@
+import random
+
+
 def count_to_infinity(start=1):
     i = start
     while True:
@@ -8,6 +11,13 @@ def count_to_infinity(start=1):
 def all_the_squares():
     for n in count_to_infinity(0):
         yield n * n
+
+
+def some_natural_numbers(p=0.1):
+    for n in count_to_infinity():
+        yield n
+        if random.uniform(0, 1) < p:
+            return
 
 
 if __name__ == '__main__':
